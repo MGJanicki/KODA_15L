8966d6e4416288033f6c74b91b6306f8486145fe
kali11 <kali.piotrek@gmail.com>
2015-05-18 00:16:36 +0200

    zrodlo o rozkladzie laplace'a

--- src/main/java/model/LaplaceSource.java
+++ src/main/java/model/LaplaceSource.java
@@ -2,12 +2,26 @@ package model;
 
 /**
  * Created by Marcin Janicki on 2015-05-12.
+ *
+ * mi - okrela przesuniecie rozkladu wzgledem zera.
+ * b - parametr skalujacy
  */
-public class LaplaceSource extends Source {
+public class LaplaceSource extends RandomSource {
+
+    private Double mi;
+    private Double beta;
+
+    public LaplaceSource(Double m, Double b) {
+        mi = m;
+        beta = b;
+    }
 
     @Override
     public int getNextValue() {
-        return 0;
+        // liczba losowa rozkladu normalnego z przedzialu (-0.5, 0.5]
+        Double uniformRandom = random.nextDouble() - 0.5;
+        Double value = mi - beta * Math.signum(uniformRandom) * Math.log(1 - 2 * Math.abs(uniformRandom));
+        return Math.round(Math.round(value));
     }
 
 }
